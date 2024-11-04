e6c56163246cd59a3f821d28296f86dce2a1e1b0
Hải Nguyễn <152304109+hainguyen312@users.noreply.github.com>
2024-11-04 21:16:30 +0700

    update backend client

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 
+
 require('dotenv').config();
 
 const { app, server } = require('./socket');
