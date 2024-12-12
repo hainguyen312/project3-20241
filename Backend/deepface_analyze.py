cf7054d2686ef407894af56663b6ed51f2c73099
Hải Nguyễn <152304109+hainguyen312@users.noreply.github.com>
2024-12-12 15:15:09 +0700

    bb

--- Backend/deepface_analyze.py
+++ Backend/deepface_analyze.py
@@ -7,6 +7,8 @@ import requests
 from PIL import Image
 from io import BytesIO
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 # Hàm tính toán cosine similarity giữa 2 embeddings
 def cosine_similarity(embedding1, embedding2):
     return np.dot(embedding1, embedding2) / (np.linalg.norm(embedding1) * np.linalg.norm(embedding2))
